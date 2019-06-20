d3adee20183d865208e1635f9b298b69cd371d1d
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-06-19 19:00:01 -0700

    debugged (I think)

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Controls from './Controls';
-import { render, fireEvent } from '@testing-library/react';
-import '@testing-library/react/cleanup-after-each'; 
+import React from "react";
+import ReactDOM from "react-dom";
+import Controls from "./Controls";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/react/cleanup-after-each";
 
-
-describe('lock button', () => {
-    it('should not work while gate is open', () => {
-        const component = render(<Controls closed={false} locked={false} />);
-        const button = component.getByText(/lock gate/i);
-        expect(button).toBeNull();
-
-    });
-    it('changes text to unlocked when gate unlocked', () => {
-        const spy = jest.fn();
-        const component = render(<Controls closed={true} locked={false} />);
-        const button = component.getByText(/lock gate/i);
-        fireEvent.click(button);
-        expect(button).toContain('Unlock Gate');
-    })
-})
+describe("lock button", () => {
+  it("should not work while gate is open", () => {
+    const toggleLocked = jest.fn();
+    const { getByText } = render(
+      <Controls toggleLocked={toggleLocked} closed={false} locked={false} />
+    );
+    const button = getByText(/lock gate/i);
+    fireEvent.click(button);
+    expect(toggleLocked).toBeCalledTimes(0);
+  });
+  it("changes text to unlocked when gate unlocked", () => {
+    const toggleLocked = jest.fn();
+    const { getByText } = render(
+      <Controls closed={true} locked={false} toggleLocked={toggleLocked} />
+    );
+    const button = getByText(/lock gate/i);
+    fireEvent.click(button);
+    expect(toggleLocked).toBeCalledTimes(1);
+  });
+});
